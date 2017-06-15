874c9ff69540f0407fa531e8c94d76d3a1301fe6
Lauren Greenberg <lgreenberg23@gmail.com>
2017-06-15 17:31:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  number_of_elements = array.length
+  if number_of_elements == 1
+    array[0]
+  elsif number_of_elements == 2
+    array.join(" and ")
+  elsif number_of_elements > 2
+    counter = 0
+    string = ""
+    while counter < number_of_elements - 1
+      string += array[counter] + ", "
+      counter += 1
+    end
+    #string << array[-2]
+    string << "and "
+    string << array[-1]
+    string
+  else
+  end
 end
